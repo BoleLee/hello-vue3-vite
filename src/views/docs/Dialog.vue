cac80d9e295ad463169b96972cfb3f742ed2b203
Grace Leeau <964624188@qq.com>
2021-04-15 18:51:34 +0800

    初始化docs/Button,Dialog,Tabs使得使用路由存在

---
+++ src/views/docs/Dialog.vue
@@ -0,0 +1,20 @@
+<template>
+<div>
+  Dialog文档
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Dialog',
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  
+</style>
